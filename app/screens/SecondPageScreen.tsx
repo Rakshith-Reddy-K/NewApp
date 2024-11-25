6b5090f2da602db3c29441430cca50c1fdb2ee63
rakshithreddy <kokonda.r@northeastern.edu>
2024-11-25 13:14:57 -0500

    add timer and promise to second page

--- app/screens/SecondPageScreen.tsx
+++ app/screens/SecondPageScreen.tsx
@@ -1,8 +1,8 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
 import { observer } from "mobx-react-lite"
-import { TextStyle, ViewStyle } from "react-native"
+import { TextStyle, View, ViewStyle } from "react-native"
 import { AppStackScreenProps } from "@/navigators"
-import { Screen, Text, TextField } from "@/components"
+import { Button, Screen, Text, TextField } from "@/components"
 import { ThemedStyle } from "@/theme"
 import { useAppTheme } from "@/utils/useAppTheme"
 import { useStores } from "@/models"
@@ -20,19 +20,52 @@ export const SecondPageScreen: FC<SecondPageScreenProps> = observer(
     const { navigation } = _props
     const { userStore } = useStores()
     const { themed } = useAppTheme()
+    const [buttonText, setButtonText] = useState("Click to start a promise")
+    const [timer, setTimer] = useState(100)
+    const promiseWithInterval = () => {
+      return new Promise((resolve) => {
+        let count = 0
+        const intervalId = setInterval(() => {
+          count++
+          if (count === 3) {
+            clearInterval(intervalId)
+            setButtonText("Finished")
+            resolve("Interval")
+          }
+        }, 1000)
+      })
+    }
+
+    const timerDecrement = () => {
+      setTimeout(() => {
+        setTimer((prevCount) => prevCount - 1)
+      }, 1000)
+    }
+    useEffect(() => {
+      timerDecrement()
+    }, [timer])
     return (
       <Screen style={$root} preset="scroll" safeAreaEdges={["top", "left", "right", "bottom"]}>
-        <Text
-          style={themed($linkToMainPage)}
-          text="Go to main page"
-          onPress={() => navigation.pop()}
-        />
+        <View style={$links}>
+          <Text
+            style={themed($linkToMainPage)}
+            text="Go to main page"
+            onPress={() => navigation.pop()}
+          />
+          <Text
+            style={themed($linkToThirdPage)}
+            text="Go to third Page"
+            onPress={() => navigation.push("ThirdPage")}
+          />
+        </View>
         <Text text="SecondPage" />
         <TextField
           style={themed($textBox)}
           label="User Data"
           value={JSON.stringify(userStore.userList)}
         ></TextField>
+        <Text text={"Timer " + timer} />
+        <Button text={buttonText} onPress={promiseWithInterval} />
       </Screen>
     )
   },
@@ -42,11 +75,15 @@ const $root: ViewStyle = {
   flex: 1,
 }
 
-// const $linkToThirdPage: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
-//   color: colors.tint,
-//   marginBottom: spacing.lg,
-//   alignSelf: "flex-end",
-// })
+const $links: ViewStyle = {
+  flexDirection: "row",
+  justifyContent: "space-between",
+}
+const $linkToThirdPage: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
+  color: colors.tint,
+  marginBottom: spacing.lg,
+  alignSelf: "flex-end",
+})
 
 const $linkToMainPage: ThemedStyle<TextStyle> = ({ colors, spacing }) => ({
   color: colors.tint,
